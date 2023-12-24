2268c784b02754a5be6d75900bedf593bc593bbd
Alexey Grishankov <domian844@gmail.com>
2023-12-24 16:29:39 +0300

    start add integration with visual prospecting

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("com.github.GTNewHorizons:CodeChickenCore:1.1.13:dev")
     implementation("com.github.GTNewHorizons:NotEnoughItems:2.4.13-GTNH:dev")
     api("space.impact:forgelin:2.0.+") { isChanging = true }
+    implementation(fileTree(mapOf("dir" to "libs/", "include" to listOf("*.jar"))))
 }
 
 val modId: String by extra
